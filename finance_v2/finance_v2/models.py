08a66a41fc9abaccb3b8f8cd030bdb10f97c7a17
balalay12 <balalay12@yandex.ru>
2015-01-06 16:58:28 +0200

    hmm2

--- finance_v2/finance_v2/models.py
+++ finance_v2/finance_v2/models.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-#TO DO разработать структуру БД
+    
+class Categorys(models.Model):
+    name = models.CharField()
+    operation_type = models.IntegerField(max_length=1)
+    
+    def __unicode__(self):
+        return self.name
+    
+class Operations(models.Model):
+    user = models.ManyToManyField('auth.User', related_name='+')
+    money = models.FloatField(verbose_name=u'Сумма')
+    date = models.DateField(verbose_name=u'Дата')
+    comment = models.CharField(verbose_name=u'Описание')
+    category = models.ForeignKey(Categorys, verbose_name=u'Категория')
+    
+    def __unicode__(self):
+        return u'%s %s %s %s' % (self.money, self.date, self.comment, self.category)

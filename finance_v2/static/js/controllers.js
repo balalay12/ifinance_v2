8cec2df3ed79938ecee43f62a1db6b04659c8328
balalay12 <balalay12@yandex.ru>
2015-01-07 19:00:57 +0200

    hmm4

--- finance_v2/static/js/controllers.js
+++ finance_v2/static/js/controllers.js
@@ -1,17 +1,20 @@
 angular.module('reg_app', [])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    })
 	.controller('RegFormController', ['$scope', '$http', function($scope, $http) {
 		$scope.submit = function() {
-			var in_data = { user: $scope.user}
-			console.log(in_data)
+			var in_data = { user: $scope.user};
+			console.log(in_data);
 			$http.post('/reg/', in_data)
 				.success(function(out_data) {
-					console.log("OK")
+					console.log("OK");
 				})
 				.error(function(out_data) {
-					console.log("NOT OK")
-				})
-		}
-	}])
+					console.log("NOT OK");
+				});
+		};
+	}]);
 
 // reg_app.controller('RegFormController', function($scope) {
 // 	$scope.submit = function() {
